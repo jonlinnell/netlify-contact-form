33f9ed206d67775b358b47bdc6f1b3de88ed7691
Jon Linnell <jon.linnell@thirdbridge.com>
2021-05-11 17:17:35 +0100

    Use creds, strip debug

--- functions/send-contact-email.js
+++ functions/send-contact-email.js
@@ -1,10 +1,21 @@
 const { SESClient, SendEmailCommand } = require("@aws-sdk/client-ses");
 
-const { RECIPIENT, MAILER_ADDRESS } = process.env;
+const {
+  RECIPIENT,
+  MAILER_ADDRESS,
+  MAILER_AWS_ACCESS_KEY_ID,
+  MAILER_AWS_SECRET_ACCESS_KEY,
+} = process.env;
 
 exports.handler = async (event) => {
   try {
-    const client = new SESClient({});
+    const client = new SESClient({
+      region: "eu-west-1",
+      credentials: {
+        accessKeyId: MAILER_AWS_ACCESS_KEY_ID,
+        secretAccessKey: MAILER_AWS_SECRET_ACCESS_KEY,
+      },
+    });
 
     const { name, email, message } = JSON.parse(event.body);
 
@@ -20,9 +31,6 @@ exports.handler = async (event) => {
       statusCode: 200,
       body: JSON.stringify({
         response: sendResponse,
-        source,
-        destination,
-        message,
       }),
     };
   } catch (err) {
@@ -30,7 +38,6 @@ exports.handler = async (event) => {
       statusCode: 500,
       body: JSON.stringify({
         message: err.toString(),
-        env: process.env,
         err,
       }),
     };
