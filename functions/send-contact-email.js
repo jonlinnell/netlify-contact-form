a333a03f7e77e0885f29293e6dd29c6e7cb852f4
Jon Linnell <jon.linnell@thirdbridge.com>
2021-05-11 15:34:08 +0100

    Stringify test output

--- functions/send-contact-email.js
+++ functions/send-contact-email.js
@@ -1,9 +1,9 @@
 exports.handler = async (event) => {
   return {
     statusCode: 200,
-    body: {
+    body: JSON.stringify({
       message: "Hit the endpoint!",
       event,
-    },
+    }),
   };
 }
